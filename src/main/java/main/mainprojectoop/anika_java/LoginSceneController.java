7ab8a4f4f5a9862e18d9a2ba4d358f460a7c3e55
Abir Shahriar <ashariar414@gmail.com>
2024-07-17 21:56:50 +0600

    changes_2

---
+++ src/main/java/main/mainprojectoop/anika_java/LoginSceneController.java
@@ -0,0 +1,16 @@
+package main.mainprojectoop;
+
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginSceneController
+{
+    @javafx.fxml.FXML
+    private TextField idtextfield;
+    @javafx.fxml.FXML
+    private PasswordField loginpassfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
